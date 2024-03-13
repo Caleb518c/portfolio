ba55ac73d8e01c5a4d92bca813c34f90fc6fc0ef
Caleb Clapper <104874097+Caleb518c@users.noreply.github.com>
2024-03-12 23:30:13 -0400

    eep time

---
+++ src/Components/ProjectTile.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+
+interface ProjectTileProps {
+  imgSrc: string;
+  imgAltText: string;
+  title: string;
+  description: string;
+  tagsArray: string[];
+}
+
+const ProjectTile: FC<ProjectTileProps> = ({
+  imgSrc,
+  imgAltText,
+  title,
+  description,
+  tagsArray,
+}) => {
+  return (
+    <div className="projectTile">
+      <img src={imgSrc} alt={imgAltText} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <ul>
+        <li>{tagsArray[0]}</li>
+        <li>{tagsArray[1]}</li>
+        <li>{tagsArray[2]}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ProjectTile;
